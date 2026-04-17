7f082dfec31a1d3e5c29f0496e406a9ecce7bd6e
youkorr <96953496+youkorr@users.noreply.github.com>
2026-04-17 20:41:23 +0200

    Update sphere_viz.cpp

--- components/sphere_viz/sphere_viz.cpp
+++ components/sphere_viz/sphere_viz.cpp
@@ -325,7 +325,7 @@ void SphereViz::render_frame_() {
   const int oy = this->h_ / 2;
 
   // Clear to opaque black
-  this->clear_buffer_(0xD97757);
+  this->clear_buffer_(0xFF000000);
 
   // Extract color channels from user-configured color
   const uint8_t CR = (this->color_ >> 16) & 0xFF;
